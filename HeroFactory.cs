881ad5d6256c7631d1fee64deb0855b20206fe85
Bora KAŞMER <bora@borakasmer.com>
2023-07-27 03:29:07 +0300

    Add HeroFactory

---
+++ HeroFactory.cs
@@ -0,0 +1,30 @@
+﻿using AloneInTheRoom.Positions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AloneInTheRoom
+{
+    public class HeroFactory
+    {
+        private int x;
+        private int y;
+        private Point roomSize;
+        private PositonEnum positon;
+        public HeroFactory() { }
+
+        public IHero CreateRobot(Point _roomSize, int _x, int _y, PositonEnum _positon)
+        {
+            x = _x;
+            y = _y;
+            positon = _positon;
+            roomSize = _roomSize;
+            IHero hero = new Hero(x, y, roomSize);
+            hero.Positon = PostionFactory.GeneratePositon(hero, positon);
+            return hero;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,10 +3,17 @@ using AloneInTheRoom;
 using AloneInTheRoom.Positions;
 using System.Drawing;
 
-Point roomSize = new(5, 5);
+/*Point roomSize = new(5, 5);
 (int x, int y, Point roomSize) heroProperties = (1, 2, roomSize);
 var hero = new Hero(heroProperties.x, heroProperties.y, heroProperties.roomSize);
 hero.Positon = PostionFactory.CreatePositon(PositonEnum.N, hero);
+*/
+HeroFactory hf = new();
+(int width, int height) roomSize = (5, 5);
+(int x, int y, string Positon) robotProperties = (1, 2, "N");
+IHero hero = hf.CreateRobot(new(roomSize.width, roomSize.height), robotProperties.x, robotProperties.y, robotProperties.Positon.GetPositonEnum());
+
+
 string moveList = "LMLMLMLMM";
 hero.MoveHero(moveList);
 Console.WriteLine($"{hero.X},{hero.Y},{hero.Positon.CurrentPositon()}");
